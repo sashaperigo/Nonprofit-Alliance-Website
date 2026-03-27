57b6bc171c76ca4fc23b76b45995737867c1d79c
sashaperigo <sasha.perigo@gmail.com>
2026-03-27 13:44:20 -0700

    Sped up scroll

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }, {
-      threshold: 0.12,
-      rootMargin: '0px 0px -40px 0px'
+      threshold: 0.05,
+      rootMargin: '0px 0px 0px 0px'
     });
 
     fadeElements.forEach((el, i) => {
